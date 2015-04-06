4115a4f2744907b49a2c147a35e44e3aa422c7bb
johnvmt <johnvmt@gmail.com>
2015-04-05 23:47:34 -0400

    Test commit

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 var config = {};
 
+config.platform = {
+	npm: "node-plumber-ec2@https://github.com/johnvmt/node-plumber-ec2.git"
+};
+
+config.platform.params = require("./ec2-creds.json");
+
 module.exports = config;
--- server.js
+++ server.js
@@ -1,1 +1,41 @@
+var npm = require("npm");
+
+
+
+
+
+npm.load({
+	loaded: false
+}, function (err) {
+	// catch errors
+	npm.commands.install(["node-plumber-ec2@git://github.com/johnvmt/node-plumber-ec2.git"], function (er, data) {
+		if(er)
+			console.log("ERR");
+		else {
+			console.log("SUCCESS");
+			console.log(data);
+			var ec2 = require('node-plumber-ec2');
+			console.log(ec2);
+//			var request = require("request");
+		}
+		//console.log("data", er, data);
+		// log the error or data
+	});
+	npm.on("log", function (message) {
+		// log the progress of the installation
+		//console.log("log", message);
+	});
+});
+
+/*
+npm.load(myConfigObject, function (er) {
+	if (er) return handlError(er)
+	npm.commands.install(["some", "args"], function (er, data) {
+		if (er) return commandFailed(er)
+		// command succeeded, and data might have some info
+	})
+	npm.registry.log.on("log", function (message) { .... })
+});
+*/
+
 
